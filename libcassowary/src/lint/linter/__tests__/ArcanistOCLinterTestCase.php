efc246ebe7643bfefd5b8bc61cdbc5f861bd53e1
Matt Bishop <mbishop@imobile3.com>
2014-02-19 16:44:09 -0500

    OCLint test update
    Summary: We don't use OCLint for line length checks anymore because our mobile lint engine takes care of it. Removes the tests for line length.
    
    Test Plan: Run `arc unit`, note tests are gone.
    
    Reviewers: ablackwood
    
    Reviewed By: ablackwood
    
    Differential Revision: https://devportal.imobile3.com/D7908

--- libcassowary/src/lint/linter/__tests__/ArcanistOCLinterTestCase.php
+++ libcassowary/src/lint/linter/__tests__/ArcanistOCLinterTestCase.php
@@ -2,18 +2,15 @@
 
 final class ArcanistOCLinterTestCase extends ArcanistLinterTestCase {
 
-	/**
- 	 * Tests the 120-character line limit advice
- 	 */
-	public function testOCLint() {
-		$linter = new ArcanistOCLinter();
-	    $working_copy = ArcanistWorkingCopyIdentity::newFromPath(__FILE__);
-		return $this->executeTestsInDirectory(
-			dirname(__FILE__) . '/objc/tests/',
-			$linter,
-			$working_copy);
-	}
-
-	public function testSomething() {
-	}
+    /**
+     * Tests the 120-character line limit advice
+     */
+    public function testOCLint() {
+        $linter = new ArcanistOCLinter();
+        $working_copy = ArcanistWorkingCopyIdentity::newFromPath(__FILE__);
+        return $this->executeTestsInDirectory(
+            dirname(__FILE__) . '/objc/tests/',
+            $linter,
+            $working_copy);
+    }
 }
--- libcassowary/src/lint/linter/__tests__/objc/oclint.sh
+++ libcassowary/src/lint/linter/__tests__/objc/oclint.sh
@@ -6,7 +6,7 @@ SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/D
 INCLUDES=''
 for folder in `find . -type d`
 do
-  INCLUDES="$INCLUDES -I$folder"
+INCLUDES="$INCLUDES -I$folder"
 done
 
-oclint -rc=LONG_LINE=120 $1 -- -x objective-c -arch $ARCH -F . -isysroot $SYSROOT -g -I$INCLUDES -c
+oclint -rc NUMBER_OF_PARAMETERS=10 -rc METHOD_LENGTH=180 $1 -- -x objective-c -arch $ARCH -F . -isysroot $SYSROOT -g -I$INCLUDES -c
