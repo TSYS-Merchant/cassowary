71f9666a86453e8eea6d11cd3c90e557b59bb266
Matt Bishop <mbishop@imobile3.com>
2014-02-19 16:58:35 -0500

    Copyright updates

--- libcassowary/src/unit/OCUnitTestEngine.php
+++ libcassowary/src/unit/OCUnitTestEngine.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Copyright 2013 iMobile3, LLC. All rights reserved.
+Copyright 2012-2014 iMobile3, LLC. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, is permitted provided that adherence to the following
