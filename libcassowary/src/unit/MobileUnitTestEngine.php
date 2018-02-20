68af2ff6c0edafab0a23a533083aa297fae9169c
Patrick Honkonen <phonkonen@imobile3.com>
2018-02-20 09:16:42 -0500

    Added Support for Gradle Unit Test Execution
    Summary: Modified unit test scripts to handle gradle projects. Arcanist will now execute local unit tests (not instrumented tests) during the diff process. If failures are encountered the diff process is stopped.
    
    Test Plan: perform arc diff --no-diff to preview the new diff process for gradle projects. Verify passing unit tests allow diff to continue uploading. Confirm failed or broken unit test stop the diff. In practice arcanist should prompt for a failure reason and still continue with the upload but it currently chokes when parsing the failed/broken result object.
    
    Reviewers: mbishop, mportune, vsalucci, arodriguez, jdunnivant
    
    Reviewed By: mbishop
    
    Differential Revision: https://phab.imobile3.local/D6298

--- libcassowary/src/unit/MobileUnitTestEngine.php
+++ libcassowary/src/unit/MobileUnitTestEngine.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Copyright 2012-2017 iMobile3, LLC. All rights reserved.
+Copyright 2012-2018 iMobile3, LLC. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, is permitted provided that adherence to the following
@@ -46,8 +46,11 @@ final class MobileUnitTestEngine extends ArcanistUnitTestEngine {
         $ios_test_paths = array();
         $android_test_paths = array();
         $dotnet_test_paths = array();
+        $current_user = get_current_user();
 
         // Looking for project root directory
+
+        // iOS (xcode)
         foreach ($this->getPaths() as $path) {
             $root_path = $this->projectRoot.'/'.$path;
 
@@ -68,25 +71,23 @@ final class MobileUnitTestEngine extends ArcanistUnitTestEngine {
             } while ($last);
         }
 
+        // Android (gradle)
         foreach ($this->getPaths() as $path) {
             $root_path = $this->projectRoot.'/'.$path;
 
             // Checking all levels of path
             do {
-                // Project root should have AndroidManifest.xml
-                // We only want projects that have tests
+                // Project root should have gradlew batch file
                 // Only add path once per project
-                if (file_exists($root_path.'/AndroidManifest.xml')
-                        && file_exists($root_path.'/tests')
-                        && !in_array($root_path, $android_test_paths)
-                ) {
+                if (file_exists($root_path.'/gradlew')
+                        && !in_array($root_path, $android_test_paths)) {
+
                     array_push($android_test_paths, $root_path);
                 }
 
-                // Stripping last level
                 $last = strrchr($root_path, '/');
                 $root_path = substr_replace($root_path, '',
-                    strrpos($root_path, $last), strlen($last));
+                    strpos($root_path, $last), strlen($last));
             } while ($last);
         }
 
@@ -112,16 +113,20 @@ final class MobileUnitTestEngine extends ArcanistUnitTestEngine {
         }
 
         // Checking to see if no paths were added
-        if (count($ios_test_paths) == 0 && count($android_test_paths) == 0
+        if (count($ios_test_paths) == 0
+                && count($android_test_paths) == 0
                 && count($dotnet_test_paths) == 0) {
             throw new ArcanistNoEffectException('No tests to run.');
         }
 
-        if (count($ios_test_paths) == 0 && count($android_test_paths) > 0) {
+        if (count($ios_test_paths) == 0
+                    && count($android_test_paths) > 0) {
             $this->androidOnly = true;
         }
 
         // Trying to build for every project
+
+        // iOS
         foreach ($ios_test_paths as $path) {
             chdir($path);
 
@@ -174,203 +179,49 @@ final class MobileUnitTestEngine extends ArcanistUnitTestEngine {
             }
         }
 
+        // Android (gradle)
         if (count($android_test_paths) > 0) {
-            foreach ($android_test_paths as $path) {
-                // Checking For and Updating Library Projects
-                $library_paths = array();
-                chdir($path);
-                $properties = file('project.properties', FILE_SKIP_EMPTY_LINES);
-                foreach ($properties as $item) {
-                    if (strpos($item, 'android.library.reference') !== false) {
-                        $library_path = substr($item, strpos($item, '=') + 1);
-                        $library_path = realpath(rtrim($library_path));
-                        array_push($library_paths, $library_path);
-                    }
-                }
-                if (count($library_paths) > 0) {
-                    foreach ($library_paths as $library_path) {
-                        chdir($library_path);
-                        list($err, $stdout, $stderr) =
-                                exec_manual('android update project --path . --subprojects');
-                    }
-                }
 
-                // Building Main Package
-                chdir($path);
-                exec('android update project --path .');
-
-                $output = array();
-                $result = 0;
-                exec('ant clean debug -d', $output, $result);
-
-                if ($result != 0) {
-                    print_r($output);
-                    throw new RuntimeException('Unable to build using [ant debug]');
-                }
-
-                // Building Test Package
-                chdir($path.'/tests');
-                exec('android update test-project --path . -m ..');
-                exec('ant clean debug -d', $output, $result);
-
-                if ($result != 0) {
-                    print_r($output);
-                    throw new RuntimeException('Unable to build using [ant debug]');
-                }
-            }
-
-            $devices = array();
-            $no_device_message_shown = false;
-            do {
-                list($err, $out) = exec_manual('adb devices');
-                $lines = explode("\n", $out);
-                foreach ($lines as $line) {
-                    $split = explode("\t", trim($line));
-                    if (count($split) > 1) {
-                        $devices[] = $split[0];
-                    }
-                }
-
-                if (!$no_device_message_shown && count($devices) == 0) {
-                    echo "No device attached. Waiting for device...\n";
-                    $no_device_message_shown = true;
-                }
-            } while (count($devices) == 0);
-
-            $device_id = $devices[0];
-
-            // Installing packages
-            foreach ($android_test_paths as $path) {
-                // Installing Main Package
-                chdir($path.'/bin');
-                list($result, $out) =
-                        exec_manual('adb -s %s install -r *-debug.apk',
-                            $device_id);
-                if ($result != 0) {
-                    $msg = $out;
-                    $matches = null;
-                    if (preg_match('/Failure \[?(\w+)\]?/', $out, $matches) > 0
-                    ) {
-                        $msg = $matches[1];
-                    }
-                    throw new RuntimeException('Unable to install app APK: '
-                    .$msg);
-                }
-
-                // Installing test package
-                chdir($path.'/tests/bin');
-                list($result, $out) =
-                        exec_manual('adb -s %s install -r *-debug.apk',
-                            $device_id);
-                if ($result != 0) {
-                    $msg = $out;
-                    $matches = null;
-                    if (preg_match('/Failure \[?(\w+)\]?/', $out, $matches) > 0
-                    ) {
-                        $msg = $matches[1];
-                    }
-                    throw new RuntimeException('Unable to install test APK: '
-                    .$msg);
-                }
-            }
-
-            // Running tests after parsing test package name
-            $descriptorspec = array(
-                0 => array('pipe', 'r'),
-                1 => array('pipe', 'w'),
-                2 => array('pipe', 'w'),
-            );
+            $output_array = array();
+            $return_value = 0;
             $result_array = array();
+
+            $result = null;
+            $start_time = null;
+            $end_time = null;
+            $test_duration = null;
+
             foreach ($android_test_paths as $path) {
-                chdir($path.'/tests');
+                $module = substr($path, strrpos($path, '/') + 1);
 
-                $xml = simplexml_load_string(
-                    file_get_contents('AndroidManifest.xml'));
+                $start_time = microtime(true);
 
-                $test_package = $xml->attributes()->package;
+                passthru('./gradlew test', $return_value);
 
-                $test_command = "adb -s $device_id shell am instrument -r -w "
-                        .$test_package."/android.test.InstrumentationTestRunner";
+                $end_time = microtime(true);
 
-                $pipes = null;
-                $process = proc_open($test_command, $descriptorspec, $pipes,
-                    realpath('./'));
+                $test_duration = ($end_time - $start_time) * 1000;
 
-                $test_number = 1;
-                $test_total = 0;
-                $test_class = '';
-                $test_name = '';
-                $user_data = null;
-                $result = null;
+                $result = new ArcanistUnitTestResult();
 
-                while ($line = fgets($pipes[1])) {
-                    if (!preg_match('/^INSTRUMENTATION_(\w+): (.*?)$/',
-                        trim($line),
-                        $matches)
-                    ) {
-                        continue;
-                    }
+                $result
+                    ->setName("$module")
+                    ->setUserData($current_user)
+                    ->setDuration($test_duration);
 
-                    if ($matches[1] == 'STATUS_CODE') {
-                        $status = $matches[2];
-                        if ($status == 0) {
-                            echo "\033[42m\033[1m PASS \033[0m\033[0m\n";
-
-                            $result = new ArcanistUnitTestResult();
-                            $result
-                                    ->setResult(ArcanistUnitTestResult::RESULT_PASS)
-                                    ->setName($test_name);
-                            $result_array[] = $result;
-
-                            $test_class = '';
-                            $test_name = '';
-                        } else if ($status == -1) {
-                            echo "\033[41m\033[1m BROKEN \033[0m\033[0m\n";
-
-                            $result = new ArcanistUnitTestResult();
-                            $result
-                                    ->setResult(ArcanistUnitTestResult::RESULT_BROKEN)
-                                    ->setName($test_name);
-                            $result_array[] = $result;
-
-                            $test_class = '';
-                            $test_name = '';
-                        } else if ($status == 1) {
-                            echo '('.$test_number.'/'.$test_total.') '.
-                                    $test_name.' in '.$test_class.'...';
-                        }
-                    } else if ($matches[1] == 'STATUS') {
-                        $fields = explode('=', $matches[2]);
-                        if (count($fields) == 2) {
-                            if ($fields[0] == 'current') {
-                                $test_number = $fields[1];
-                            } else if ($fields[0] == 'numtests') {
-                                $test_total = $fields[1];
-                            } else if ($fields[0] == 'class') {
-                                $test_class = $fields[1];
-                            } else if ($fields[0] == 'test') {
-                                $test_name = $fields[1];
-                            }
-                        }
-                    } else if ($matches[1] == 'RESULT') {
-                        $fields = explode('=', $matches[2]);
-                        if (count($fields) == 2) {
-                            if ($fields[0] == 'longMsg') {
-                                echo "\033[41m\033[1m FAIL \033[0m\033[0m: "
-                                        .$fields[1]."\n";
-
-                                $result = new ArcanistUnitTestResult();
-                                $result
-                                        ->setResult(ArcanistUnitTestResult::RESULT_FAIL)
-                                        ->setName($test_name);
-                                $result_array[] = $result;
-                            }
-                        }
-                    } else if ($matches[1] == 'CODE') {
-                        // 0 == fail?
-                    }
+                if ($return_value == 0) {
+                    $result
+                        ->setResult(ArcanistUnitTestResult::RESULT_PASS);
+                    $result_array[] = $result;
+                } else if ($return_value == -1) {
+                    $result
+                        ->setResult(ArcanistUnitTestResult::RESULT_BROKEN);
+                    $result_array[] = $result;
+                } else if ($return_value == 1) {
+                    $result
+                        ->setResult(ArcanistUnitTestResult::RESULT_FAIL);
+                    $result_array[] = $result;
                 }
-                proc_close($process);
             }
         }
 
